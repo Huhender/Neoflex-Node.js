3bafb806af1c59bf338a5cdff878272630dae616
Huhender <114951180+Huhender@users.noreply.github.com>
2022-10-03 22:02:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+module.exports = function (server) {
+
+    
+    server.route({
+        method: 'GET',
+        path: '/calculadora/about',
+        handler: function (request, h) {
+
+            var data = {
+                message: 'Calculator API - made by Bernardo Rocha'
+            };
+
+            return data;
+        }
+    });
+
+    
+    server.route({
+        method: 'GET',
+
+        path: '/calculator/sum/{num1}+{num2}',
+        handler: function (request, h) {
+
+            const num1 = parseInt(request.params.num1);
+            const num2 = parseInt(request.params.num2);
+
+            var data = {
+                answer: num1 + num2
+            };
+
+            return data;
+        }
+    });
+
+    
+    server.route({
+        method: 'GET',
+
+        path: '/calculator/sub/{num1}-{num2}',
+        handler: function (request, h) {
+
+            const num1 = parseInt(request.params.num1);
+            const num2 = parseInt(request.params.num2);
+
+            var data = {
+                answer: num1 - num2
+            };
+
+            return data;
+        }
+    });
+
+    
+    server.route({
+        method: 'GET',
+
+        path: '/calculator/multi/{num1}*{num2}',
+        handler: function (request, h) {
+
+            const num1 = parseInt(request.params.num1);
+            const num2 = parseInt(request.params.num2);
+
+            var data = {
+                answer: num1 * num2
+            };
+
+            return data;
+        }
+    });
+
+    
+    server.route({
+        method: 'GET',
+
+        path: '/calculator/div/{num1}/{num2}',
+        handler: function (request, h) {
+
+            const num1 = parseInt(request.params.num1);
+            const num2 = parseInt(request.params.num2);
+
+            var data = {
+                answer: num1 / num2
+            };
+
+            return data;
+        }
+    });
+}
